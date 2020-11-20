49d2042047aeb09ab41a3fe6f09c17b62a4984d4
Amanda Otero <mandystar018@gmail.com>
2020-11-20 10:06:32 -0800

    Add Currency class to the currency-service.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// usually when is more things to export import {Triangle} from './triangle.js'
+
 
 $(document).ready(function() {
   $('#TODO').submit(function(event) {
