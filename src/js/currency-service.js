87e25170d01dfdf55b94e2c5ae60438c97f2b208
Amanda Otero <mandystar018@gmail.com>
2020-11-20 11:00:01 -0800

    problem with the call no getting response

--- src/js/currency-service.js
+++ src/js/currency-service.js
@@ -1,5 +1,5 @@
 export default class Currency {
-  static getCurrency(country){
+  static getCurrency(country1, country2){
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
     .then(function(response) {
       if(!response.ok) {
--- src/main.js
+++ src/main.js
@@ -4,3 +4,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Currency from './js/currency-service.js';
 
+
+function showCurrency(response) {
+  if (response.conversion_rates) {
+    $('.results').text(`This is you actual currency $${response.USD} ${response.base_code}`);
+    $('.results').text(`This is exchange currency`)
+  }
+}
+
+$('#click').click(function() {
+  const currency1 = $('#currency1').val();
+  const currency2 = $('#currency2').val();
+  Currency.getCurrency(currency1, currency2)
+    .then(function(response) {
+      showCurrency(response);
+    });
+});
