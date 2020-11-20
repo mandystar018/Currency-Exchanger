b37bfcf5ce6611cc56166acc3a18a34a4bf4d211
Amanda Otero <mandystar018@gmail.com>
2020-11-20 13:32:48 -0800

    Got response from API call

--- src/main.js
+++ src/main.js
@@ -7,17 +7,15 @@ import Currency from './js/currency-service.js';
 
 function showCurrency(response) {
   if (response.conversion_rates) {
-    $('.results').text(`This is you actual currency $${response.USD} ${response.base_code}`);
-    $('.results').text(`This is exchange currency`)
+    $('#results').text(`This is exchange currency $${response.conversion_rates.USD}`);
   } else {
-    $('#errors').text(`There was an error`)
+    $('#errors').text(`There was an error ${response.result}`);
   }
 }
 
 $('#click').click(function() {
   const currency1 = $('#currency1').val();
-  const currency2 = $('#currency2').val();
-  Currency.getCurrency(currency1, currency2)
+  Currency.getCurrency(currency1)
     .then(function(response) {
       showCurrency(response);
     });
