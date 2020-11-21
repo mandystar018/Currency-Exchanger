70b28ca13d148b4fcfe5b0b74978ea618c5798d9
Amanda Otero <mandystar018@gmail.com>
2020-11-20 16:29:45 -0800

    Multiply User amount with currency

--- src/main.js
+++ src/main.js
@@ -4,17 +4,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Currency from './js/currency-service.js';
 
-function calculateCurrency(amount, currency, response) {
-  
+function calculateCurrency(amount, currency){
+  amount * currency
+}
+
+function getElements(amount, currency, response) {
+  if (response.conversion_rates) {
+    $('#results').text(`This is your currency ${total} from ${response.conversion_rates.EUR}`)
+  }
   
 }
 
 $('#click').click(function() {
   const amount = $('#money').val();
-  const currency = $('#currency').val();
+  const Usercurrency = $('#currency').val();
   Currency.getCurrency()
     .then(function(response) {
-      calculateCurrency(amount, currency, response);
+      getElements(amount, Usercurrency, response);
     });
 
 });
