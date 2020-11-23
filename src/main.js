cfeb05b5afdd4883ab0941a887bf10f68d412a78
Amanda Otero <mandystar018@gmail.com>
2020-11-23 12:06:41 -0800

    Add a for of loop to iterate through the conversion_rate object

--- src/main.js
+++ src/main.js
@@ -4,16 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Currency from './js/currency-service.js';
 
-function calculateCurrency(amount, currency){
-  amount * currency
+function getElement(amount, Usercurrency){
+	for (const [key ,value] of Object.entries(obj.conversion_rates)) {
+    let result = 0;
+    let total = amount.toFixed(2) * value.toFixed(2);
+    console.log(total);
+	}
 }
-
-function getElements(amount, currency, response) {
-  if (response.conversion_rates) {
-    $('#results').text(`This is your currency ${total} from ${response.conversion_rates.EUR}`)
-  }
-  
-}
+getElement(400);
 
 $('#click').click(function() {
   const amount = $('#money').val();
