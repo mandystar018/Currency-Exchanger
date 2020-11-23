f959e8f89530bd9f7f0b204f977df3d1f393bdd3
Amanda Otero <mandystar018@gmail.com>
2020-11-23 12:51:38 -0800

    Add select bar so the user can select the exchange currency

--- src/main.js
+++ src/main.js
@@ -6,12 +6,15 @@ import Currency from './js/currency-service.js';
 
 function getElement(amount, Usercurrency){
 	for (const [key ,value] of Object.entries(obj.conversion_rates)) {
-    let result = 0;
-    let total = amount.toFixed(2) * value.toFixed(2);
-    console.log(total);
+  	let options = `<option value=${key}>${key}</option>`;
+    $('#currency').append(options);
+  	/* let result = 0;
+  	    let total = amount.toFixed(2) * value.toFixed(2);
+  	    console.log(total); */
 	}
 }
-getElement(400);
+
+getElement(500);
 
 $('#click').click(function() {
   const amount = $('#money').val();
