a747ca2e0248a943f5c6e30964f471d2772e6e72
Amanda Otero <mandystar018@gmail.com>
2020-11-23 19:05:24 -0800

    Add Math.round to the getElement function

--- src/main.js
+++ src/main.js
@@ -4,28 +4,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Currency from './js/currency-service.js';
 
-function getElement(response, amount){
+function getElement(response, amount, userCurrency){
   let key = Object.entries(response.conversion_rates).map(([key, value])=>
-    `<option value=${value}>${key}</option>`
-    ).join('');
+    `<option value=${value}>${key}</option>`);
     console.log(key);
-  // .map(element =>
-  //   `<option value=${element}>${element}</option>`
-  //   ).join('');
-  $('#currency').append(key);
-  if (key) {
-    
-  }
-  
+  $('select').append(key);
+  let total = Math.round(amount).toFixed(2) * Math.round(`${key.value}`).toFixed(2);
+  console.log(total);
   }
 
-async function makeApiCall(amount) {
+async function makeApiCall(amount, userCurrency) {
   const response = await Currency.getCurrency();
-  getElement(response,amount);
+  getElement(response, amount, userCurrency);
 }
 
 $(document).ready(function() {
   const amount = $('#money').val(); 
-  const Usercurrency = $('#currency').val();
-  makeApiCall(amount);
+  const userCurrency = $('#currency').val();
+  makeApiCall(amount, userCurrency);
 });
